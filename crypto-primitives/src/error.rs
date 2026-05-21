bd2b425bbe60e287a2fd1110c6b52d6338ba9b6b
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2026-05-21 20:12:52 +0200

    add Salt generation mechanism

--- crypto-primitives/src/error.rs
+++ crypto-primitives/src/error.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum HKDFError {
+    #[error("Salgeneration error: {0}")]
+    SaltGenerationError(String),
     #[error("IKM must not be empty")]
     EmptyIKM,
     #[error("requested output length {requested} excedes max {max}")]
--- crypto-primitives/src/lib.rs
+++ crypto-primitives/src/lib.rs
@@ -1,6 +1,38 @@
-pub mod hkdf;
-pub mod nonce;
-pub mod tag;
+use getrandom::{rand_core::TryRng, SysRng};
+use error::HKDFError;
+pub mod error;
+
+pub struct Salt(pub(crate) [u8; 32]);
+
+impl Drop for Salt {
+    fn drop(&mut self) {
+        // Zero out the salt when it goes out of scope
+        self.0.iter_mut().for_each(|byte| *byte = 0);
+    }
+}
+
+pub struct SaltGenerator {
+    rng: SysRng,
+}
+
+impl SaltGenerator {
+    pub fn new() -> SaltGenerator {
+        Self {rng: SysRng}
+    }
+
+    /// Generates a new salt of 32 bytes.
+    /// The salt is automatically zeroed out when it goes out of scope.
+    /// ensures that the salt is not accidentally reused or leaked.
+    pub fn generate_salt(&mut self) -> Result<Salt, HKDFError> {
+        let mut salt = [0u8; 32];
+        // Fill the salt with pseudorandom bytes
+        self.rng.try_fill_bytes(&mut salt)
+            .map_err(|e| HKDFError::SaltGenerationError(e.to_string()))?;
+        
+        Ok(Salt(salt))
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
