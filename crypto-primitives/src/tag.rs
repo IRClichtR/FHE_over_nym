a76dd5792e7bf3b81516a86d3082ac5b50278c4b
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2026-05-25 16:14:23 +0200

    tag

--- crypto-primitives/src/tag.rs
+++ crypto-primitives/src/tag.rs
@@ -1,17 +1,16 @@
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
 use crate::nonce::Nonce;
 use crate::types::{StealthInput, Tag, TagKey};
 
-
-pub fn derive_tag_key(shared_secret: [u8; 32], stealth_input: StealthInput, nonce: Nonce) -> TagKey {
-    unimplemented!()
-}
-
+/// Generates a tag using HMAC with the provided tag key and nonce.
 pub fn generate_tag(tag_key: TagKey, nonce: Nonce) -> Tag {
-    // Generate a tag by computing an HMAC using the tag key and nonce as input
-    unimplemented!()
+    let mut mac = Hmac::<Sha256>::new_from_slice(&tag_key.0).expect("HMAC can take key of any size");
+    mac.update(&nonce.0);
+    Tag(mac.finalize().into_bytes().into())
 }
 
 pub fn recognize_tag(tag_key: TagKey, nonce: Nonce, candidate: &Tag) -> bool {
-    // recompute HMAC with the tag key and nonce, and compare it to the candidate tag
-    unimplemented!()
+    let generated = generate_tag(tag_key, nonce);
+    generated == *candidate
 }
